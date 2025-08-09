c274fb05d1436e935ca03c72f07a3c576d6f1b6c
kangyio <kangyio1986@gmail.com>
2025-08-09 23:06:41 +0800

    initialize DB for first time use

--- src/electron/util.ts
+++ src/electron/util.ts
@@ -1,5 +1,6 @@
 import { app } from "electron";
 import fsPromises from "node:fs/promises";
+import { existsSync } from "node:fs";
 import path from "node:path";
 
 export function isDev(): boolean {
@@ -18,12 +19,31 @@ export async function writeToDB(cardInfoArray: CardInfo[], fileName: string) {
 }
 
 export async function readFromDB(fileName: string) {
-  const filePath = path.join(app.getAppPath(), "src", "electron", fileName);
-
   try {
+    const filePath = path.join(app.getAppPath(), "src", "electron", fileName);
+    if (!existsSync(filePath)) await initializeDB(filePath, fileName);
+
     return await fsPromises.readFile(filePath, "utf-8");
   } catch (err) {
     if (err instanceof Error) console.log(err.message);
     console.log(String(err));
+  }
+}
+
+async function initializeDB(filePath: string, fileName: string) {
+  const timeNow = Date.now();
+  if (fileName === "cards.json") {
+    const initContent = JSON.stringify([
+      {
+        createdAt: timeNow,
+        updatedAt: [timeNow],
+        title: "Welcome!",
+        color: "#4caf50"
+      }
+    ]);
+    await fsPromises.writeFile(filePath, initContent);
+  } else if (fileName === "cardLayout.json") {
+    const initContent = JSON.stringify([{ i: String(timeNow), x: 1, y: 0, w: 1, h: 1 }]);
+    await fsPromises.writeFile(filePath, initContent);
   }
 }
